4cbbc20164e47a6d38353441023c21ac81da89ad
Alessandro Milos <alessandro@MacBook-Air-di-Alessandro.local>
2024-12-03 17:30:37 +0100

    service manager

--- service-layer/ServiceManager.js
+++ service-layer/ServiceManager.js
@@ -12,50 +12,62 @@ export class ServiceManager {
     ServiceManager.#instance = this;
   }
 
-  mutate(request) {
+  async mutate(request) {
     const { data, remoteService, localService } = request;
 
+    let remoteRes, localRes;
+
     if (remoteService) {
-      console.log(this.__remoteManager.mutate(data, remoteService));
+      remoteRes = await this.__remoteManager.mutate(data, remoteService);
     }
     if (localService) {
-      console.log(this.__localManager.mutate(data, localService));
+      localRes = await this.__localManager.mutate(data, localService);
     }
+
+    if (remoteService && localService) {
+      return { remote: remoteRes, local: localRes };
+    }
+
+    return remoteRes || localRes; // return only the exisiting one
   }
 
-  query(request) {
+  async query(request) {
     const { remoteService, localService } = request;
 
-    if (remoteService) this.__remoteManager.query(remoteService);
-    if (localService) this.__localManager.query(localService);
+    let remoteRes, localRes;
+
+    if (remoteService) {
+      remoteRes = await this.__remoteManager.query(remoteService);
+    }
+    if (localService) {
+      localRes = await this.__localManager.query(localService);
+    }
+
+    // We need to decide who wins between the local and remote response of the queried data.
+    // 1. Merge data based on timestamps
+    // 2. If one fails, the other wins
+
+    if (remoteService && localService) {
+      // Merge goes here
+    }
   }
 }
 
 class RemoteManager {
   async mutate(data, service) {
-    try {
-      const res = await service(data);
-      console.log(res);
-    } catch (err) {
-      console.log(err);
-    }
+    return await service(data);
   }
 
   async query(service) {
-    service();
+    return await service();
   }
 }
 class LocalManager {
   async mutate(data, service) {
-    try {
-      const res = await service(data);
-      console.log(res);
-    } catch (err) {
-      console.log(err);
-    }
+    return await service(data);
   }
 
   async query(service) {
-    service();
+    return await service();
   }
 }
